75abbb9a5a8bd2ad7e5166352d33ec5833e8ac47
dialahalkhalili <70059987+dialahalkhalili@users.noreply.github.com>
2020-10-06 22:09:26 +0300

    ADD BUTTON

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 console.log('Connected')
+
+var addButton = document.getElementById('addTask') 
+
+$("#addTask").click(function(){
+	//get the value of the input task to add to the li
+	var inputTask = $('#task').val()
+	$(".list").append('<li>' + inputTask + '</li>');
+});
