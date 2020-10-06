7545b574a4f1b0421e3afb51f7e5cf59f1fecf6f
dialahalkhalili <70059987+dialahalkhalili@users.noreply.github.com>
2020-10-07 02:08:47 +0300

    Add BUTTON.2

--- main.js
+++ main.js
@@ -1,9 +1,64 @@
 console.log('Connected')
 
 var addButton = document.getElementById('addTask') 
+var deleteButton = document.getElementById('deleteTask')
 
-$("#addTask").click(function(){
+//declare a new variable for array
+var tasksArray = [];
+
+addButton.onclick = function appendList() {
+	// debugger;
+	//declare a new variable nd grab the ul list
+	var ul= document.getElementsByClassName('list')[0];
+	//create an li list to append into the ul
+	var li= document.createElement('li');
+	//set class to li
+	li.setAttribute("class", "li");
 	//get the value of the input task to add to the li
-	var inputTask = $('#task').val()
-	$(".list").append('<li>' + inputTask + '</li>');
-});
+	var inputTask= document.getElementById('task').value;
+	//set value of li to inputTask
+	li.innerHTML=inputTask;
+	//store tasks into an array
+	tasksArray.push(li.innerHTML);
+	//append li to ul
+	ul.appendChild(li);
+}
+
+
+
+// $("#addButton").click(function(){
+// 	//get the value of the input task to add to the li
+// 	var inputTask = $('#task').val()
+// 	//append the input task into an li into the ul
+// 	$(".list").append('<li>' + inputTask + '</li>');
+// 	console.log($('<li>.val()'))
+// });
+
+// var inputTask = document.getElementById('task').value;
+// var addButton = document.getElementById('addButton');
+// // var toDoList = document.getElementById('tasksToDo');
+// var ul = document.getElementsByClassName('list')[0];
+
+// addButton.onclick = function appendList() {
+// 	//create an li list to append into the ul
+// 	var li = document.createElement('li');
+// 	//set class to li
+// 	li.setAttribute("class", "li");
+// 	//set value of li to inputTask
+// 	var inputTask = document.getElementById('task').value;
+// 	li.innerHTML =inputTask;
+// 	//store tasks into an array
+// 	tasksArray.push(li.innerHTML);
+// 	//append li to ul
+// 	ul.appendChild(li);
+// }
+
+// $("#addButton").click(function(){
+// 	//get the value of the input task to add to the li
+// 	var inputTask = $('#task').val()
+// 	//append the input task into an li into the ul
+// 	$(".list").append('<li>' + inputTask + '</li>');
+// 	console.log($('<li>.val()'))
+// });
+
+
