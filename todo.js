21cf267e41c6946719fe135e6ca289cf1b31c7f8
dialahalkhalili <70059987+dialahalkhalili@users.noreply.github.com>
2020-10-07 21:12:57 +0300

    DONE AND DELETE BUTTONS

--- todo.js
+++ todo.js
@@ -8,15 +8,14 @@ var list = document.getElementById('todo-list');
 var tasksArray = [];
 
 addButton.onclick = function appendTask(){
-
 	//create div in ul for each task
 	var todoDiv = document.createElement('div');
 	//add class to div
-	todoDiv.setAttribute('id','todo-Div')
+	todoDiv.classList.add('todo-Div')
 	//create li into div
 	var todoTasks = document.createElement('li');
 	//add class to li
-	todoTasks.setAttribute('id','todo-li')
+	todoTasks.classList.add('todo-li')
 	//assign value of li to todoInput
 	todoTasks.innerText = todoInput.value;
 	//append li to div
@@ -26,7 +25,7 @@ addButton.onclick = function appendTask(){
 	//add name to button
 	doneTaskBtn.innerText = 'Done';
 	//set attribute to button
-	doneTaskBtn.setAttribute('id','taskDone')
+	doneTaskBtn.classList.add('taskDone')
 	//add button to div
 	todoDiv.appendChild(doneTaskBtn);
 	//add delete button next to task
@@ -34,21 +33,34 @@ addButton.onclick = function appendTask(){
 	//add name to button
 	deleteTaskBtn.innerText = 'Delete';
 	//set attribute to button
-	deleteTaskBtn.setAttribute('id','taskDelete')
+	deleteTaskBtn.classList.add('taskDelete')
 	//add button to div
 	todoDiv.appendChild(deleteTaskBtn);
 	//append div to existing ul
 	list.appendChild(todoDiv);
+	//alert if input value is empty
+	if(todoInput.value === '')
+		alert('Please input task')
 	//push tasks to array
 	tasksArray.push(todoTasks.innerHTML);
 	//clear input value
 	todoInput.value = '';
 }
 
-list.onclick = function doneDelButtons(){
-	console.log(event.target.nodeName)
+list.onclick = function doneDel(){
+	//get the tag of each item
+	var item = event.target;
+	//if the tag is equal to that of the delete button
+	if(item.classList[0] === 'taskDelete'){
+		//go to the whole div and delete it
+		var removeTask = item.parentElement;
+		removeTask.remove();
+	}
+
+	if(item.classList[0] === 'taskDone'){
+		var doneTask = item.parentElement;
+		//strike through the classList of the specific li
+		doneTask.classList.toggle('completed');
+	}
+
 }
-
-
-
-
